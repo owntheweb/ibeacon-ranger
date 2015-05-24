bca149fe76480c7328891c36379e67dffca09bf9
Christopher Stevens <owntheweb@gmail.com>
2015-05-24 16:47:20 -0600

    ...

--- js/index.js
+++ js/index.js
@@ -325,4 +325,23 @@ BeaconMonitor.prototype.onDeviceReady = function() {
 };
 
 //get the show started when the device is ready
-document.addEventListener('deviceready', function() { var bMonitor = new BeaconMonitor(); bMonitor.onDeviceReady(); }, false);
+document.addEventListener('deviceready', function() { 
+	var bMonitor = new BeaconMonitor(); 
+
+	//range
+	bMonitor.createRangeListMarkup();
+	bMonitor.startRangingBeacons();
+
+	//init range signal strength graph
+    //this.signalGraph = new SignalGraph();
+    //this.signalGraph.init(this.rangeBeacons.length);
+
+    //monitor
+    bMonitor.createMonitorListMarkup();
+	bMonitor.startMonitoringBeacons();
+
+	//manage iBeacon monitoring/ranging events
+    bMonitor.setDeligate();
+	
+
+}, false);
